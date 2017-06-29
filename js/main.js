8924923c62ea712eaef9b6c423f98a6fb29e0708
CY <123@qq.com>
2017-06-29 09:46:40 +0800

    最后完成了，我好开心！！！

--- js/main.js
+++ js/main.js
@@ -15,26 +15,63 @@
 /**
  *父页面调用子页面方法 
  * */
+function reset(){
+	var more = document.getElementById("iframe-article");
+	var contentAll = document.getElementById("contentAll");
+	var All = contentAll.getElementsByTagName("iframe");
+	for(var i=0;i<3;i++){
+		All[i].style.display = "block";
+	}
+	All[3].style.display = "none";
+}
+
 window.onload = function(){
 	document.getElementById('iframe-hotPhsts').contentWindow.child();
 //	document.getElementById("iframe-allPsots").contentWindow.disA();
 //	document.getElementById("iframe-allPsots").contentWindow.allPostsChild();
 }
 function parent(){
-	alert("hello,I am father!");
+//	alert("hello,I am father!");
 }
 
-//function tabHome();
-//function tabMsg();
+function tabHome(){
+	reset();
+	document.getElementById("iframe-allPsots").contentWindow.disAll();
+}
+function tabMsg(){
+	var more = document.getElementById("iframe-article");
+	var contentAll = document.getElementById("contentAll");
+	var All = contentAll.getElementsByTagName("iframe");
+	for(var i=0;i<3;i++){
+		All[i].style.display = "none";
+	}
+	more.style.display = "block";	
+	console.log("hello");
+}
 
 function tabPoetry(){
+	reset();
 	document.getElementById("iframe-allPsots").contentWindow.disB();
 }
 
 function tabMusic(){
+	reset();
 	document.getElementById("iframe-allPsots").contentWindow.disA();
 }
 
 function tabArticle(){
+	reset();
 	document.getElementById("iframe-allPsots").contentWindow.disC();
+}
+
+function more(){
+	console.log("main.js-more()");
+	var more = document.getElementById("iframe-article");
+	var contentAll = document.getElementById("contentAll");
+	var All = contentAll.getElementsByTagName("iframe");
+	for(var i=0;i<3;i++){
+		All[i].style.display = "none";
+	}
+	more.style.display = "block";	
+	console.log("main.js-more()");
 }
